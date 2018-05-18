349afadbbf66bb0d682c57b6e216f3916b01b57b
murraydo <vatandas@mail.de>
2018-05-18 08:03:45 +0200

    TestMockups hinzugefügt

---
+++ TestMockup/src/Test/Calculator.java
@@ -0,0 +1,32 @@
+package Test;
+
+/**
+ *
+ * @author Murat Vatandas
+ */
+
+    abstract class Calculator{
+
+ 
+
+   abstract void calculate();
+
+ 
+
+   public static void main(String[] args){
+
+ 
+
+      System.out.println("calculating");
+
+ 
+
+      Calculator x = null;
+
+ 
+
+      x.calculate();
+
+
+   }
+    }
---
+++ TestMockup/src/Test/Test.java
@@ -0,0 +1,33 @@
+package Test;
+
+/**
+ *
+ * @author Murat Vatandas
+ */
+class Product {
+
+    double price;
+}
+
+public class Test {
+    public void updatePrice(Product  product, double price){
+        price = price *2;
+        product.price = product.price + price;
+    }
+    
+    public static void main(String[] args) {
+        Product prt = new Product();
+        prt.price = 200;
+        double newPrice = 100;
+        
+        Test t = new Test();
+        t.updatePrice(prt, newPrice);
+        System.out.println(prt.price + " : " + newPrice);
+        // Output: 400:100;
+        //---------------------------------------------
+        int aVar = 9;
+        if(aVar++ <10){
+            
+        }
+    }
+}
